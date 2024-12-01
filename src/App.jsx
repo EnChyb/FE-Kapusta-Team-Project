e15e4bde13e464fa639aafe5f941aab8f947734b
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-12-01 10:52:50 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SharedLayout from "./components/SharedLayout/SharedLayout";
+import MainPage from "./pages/MainPage";
 
-function App() {
-    const [count, setCount] = useState(0);
-
-    return <></>;
-}
+const App = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<SharedLayout />}>
+					<Route index element={<MainPage />} />
+				</Route>
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
