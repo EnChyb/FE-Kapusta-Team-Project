416ea6c97a8680bf73490d01cb9e1bfd76cd9855
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-12-07 13:49:21 +0000

    Create BalanceModal.jsx

---
+++ src/components/BalanceModal/BalanceModal.jsx
@@ -0,0 +1,13 @@
+export const BalanceModal = () => {
+	return (
+		<div>
+			<div>
+				<p>
+					Hello! To get started, enter the current balance of your account!
+				</p>
+				<p>You can't spend money until you have it :)</p>
+			</div>
+			<div></div>
+		</div>
+	);
+};
