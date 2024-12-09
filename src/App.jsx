d53bc199ae7d925935c7eb38db7686c609eae1f1
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-12-09 14:20:53 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import SharedLayout from "./components/SharedLayout/SharedLayout";
-// import MainPage from "./pages/MainPage";
-// import HomePage from "./pages/HomePage";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<SharedLayout />}>
-//           <Route index element={<MainPage />} />
-//           <Route
-//             path="/home"
-//             element={
-//               localStorage.getItem("token") ? <HomePage /> : <MainPage />
-//             }
-//           />
-//         </Route>
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import SharedLayout from "./components/SharedLayout/SharedLayout";
@@ -32,23 +6,23 @@ const MainPage = lazy(() => import("./pages/MainPage"));
 const HomePage = lazy(() => import("./pages/HomePage"));
 
 const App = () => {
-  return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route index element={<MainPage />} />
-            <Route
-              path="/home"
-              element={
-                localStorage.getItem("token") ? <HomePage /> : <MainPage />
-              }
-            />
-          </Route>
-        </Routes>
-      </Suspense>
-    </Router>
-  );
+	return (
+		<Router>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path="/" element={<SharedLayout />}>
+						<Route index element={<MainPage />} />
+						<Route
+							path="/home"
+							element={
+								localStorage.getItem("token") ? <HomePage /> : <MainPage />
+							}
+						/>
+					</Route>
+				</Routes>
+			</Suspense>
+		</Router>
+	);
 };
 
 export default App;
