258d6c445f2b172c13d15f069c18f66f66151bef
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-12-13 15:39:52 +0000

    up

--- src/components/DataHeader/DataHeader.jsx
+++ src/components/DataHeader/DataHeader.jsx
@@ -4,28 +4,28 @@ import CurrentPeriod from "../CurrentPeriod/CurrentPeriod";
 import "./DataHeader.css";
 
 const DataHeader = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <div className="container_dataheader">
-      <Balance />
-      {location.pathname === "/home" ? (
-        <Link className="reportsLinkWrapper" to="/reports">
-          <span className="reports">Reports</span>
-          <svg className="iconReports" width="24" height="24">
-            <use href="/sprite.svg#icon-reports"></use>
-          </svg>
-        </Link>
-      ) : (
-        <>
-          <Link className="mainPageLinkWrapper" to="/home">
-            <span className="mainPage">Main page</span>
-          </Link>
-          <CurrentPeriod />
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="container_dataheader">
+			<Balance />
+			{location.pathname === "/home" ? (
+				<Link className="reportsLinkWrapper" to="/reports">
+					<span className="reports">Reports</span>
+					<svg className="iconReports" width="24" height="24">
+						<use href="/sprite.svg#icon-reports"></use>
+					</svg>
+				</Link>
+			) : (
+				<>
+					<Link className="mainPageLinkWrapper" to="/home">
+						<span className="mainPage">Main page</span>
+					</Link>
+					<CurrentPeriod />
+				</>
+			)}
+		</div>
+	);
 };
 
 export default DataHeader;
