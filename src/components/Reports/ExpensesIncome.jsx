307a651ac67f1f569626d2c548cd5eac375c67c4
miker2004 <157730940+miker2004@users.noreply.github.com>
2024-12-18 18:20:01 +0100

    useState Change
    I deleted REDUX

--- src/components/Reports/ExpensesIncome.jsx
+++ src/components/Reports/ExpensesIncome.jsx
@@ -1,39 +1,57 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './ExpensesIncome.css';
-import { fetchExpense, fetchIncome } from '../../redux/transactionsSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
 const ExpensesIncome = () => {
-  const dispatch = useDispatch();
-  const { income, expense, error } = useSelector((state) => state.transactions);
+  const [income, setIncome] = useState([]);
+  const [expense, setExpense] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    dispatch(fetchIncome());
-    dispatch(fetchExpense());
-  }, [dispatch]);
+    const fetchIncome = async () => {
+      try {
+        const response = await axios.get('https://kapusta-backend.goit.global/transaction/income');
+        setIncome(response.data.incomes);
+      } catch (err) {
+        setError(err.message || "Błąd podczas pobierania dochodów");
+      }
+    };
 
-  // if (error) return <p>Error: {error.message || error}</p>;
+    const fetchExpense = async () => {
+      try {
+        const response = await axios.get('https://kapusta-backend.goit.global/transaction/expense');
+        setExpense(response.data.expense);
+      } catch (err) {
+        setError(err.message || "Błąd podczas pobierania wydatków");
+      }
+    };
+
+    fetchIncome();
+    fetchExpense();
+  }, []);
 
   const renderIncome = () => {
     if (Array.isArray(income)) {
-      return income.reduce((sum, item) => sum + item.amount, 0); 
+      return income.reduce((sum, item) => sum + item.amount, 0);
     }
     return income; 
   };
 
   const renderExpense = () => {
     if (Array.isArray(expense)) {
-      return expense.reduce((sum, item) => sum + item.amount, 0); 
+      return expense.reduce((sum, item) => sum + item.amount, 0);
     }
     return expense; 
   };
 
+  // if (error) {
+  //   return <p>Błąd: {error}</p>;
+  // }
+
   return (
     <div className="main-expenses-income-div">
       <section className="expenses-income-section">
-        <p className="expenses-income-txt">
-          Income:
-        </p>
+        <p className="expenses-income-txt">Income:</p>
         <p className="expenses-income-txt income-extention-txt">
           + {renderIncome()} UAH
         </p>
@@ -42,9 +60,7 @@ const ExpensesIncome = () => {
       <div className="expenses-and-income-divider"></div>
 
       <section className="expenses-income-section">
-        <p className="expenses-income-txt">
-          Expenses:
-        </p>
+        <p className="expenses-income-txt">Expenses:</p>
         <p className="expenses-income-txt expenses-extention-txt">
           - {renderExpense()} UAH
         </p>
