f116f83c8cde03c97ed1a459dc4316a6ebe270b9
MaciejBratko <maciej.bratko@outlook.com>
2024-12-19 21:56:53 +0100

    widths changes

--- src/components/FinanceTable/FinanceTable.jsx
+++ src/components/FinanceTable/FinanceTable.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./FinanceTable.css";
 
 const FinanceTable = ({ data }) => {
@@ -15,32 +14,36 @@ const FinanceTable = ({ data }) => {
   ];
 
   return (
-    <table className="finance-table">
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Description</th>
-          <th>Category</th>
-          <th>Sum</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tableData.map((entry, index) => (
-          <tr key={index}>
-            <td>{entry.date}</td>
-            <td>{entry.description}</td>
-            <td>{entry.category}</td>
-            <td
-              className={
-                entry.amount < 0 ? "negative-amount" : "positive-amount"
-              }
-            >
-              {entry.amount === null ? "" : entry.amount.toFixed(2)}
-            </td>
+    <div className="finance-table-container">
+      <table className="finance-table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Sum</th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {tableData.map((entry, index) => (
+            <tr key={index}>
+              <td>{entry.date}</td>
+              <td>{entry.description}</td>
+              <td>{entry.category}</td>
+              <td
+                className={
+                  entry.amount < 0 ? "negative-amount" : "positive-amount"
+                }
+              >
+                {entry.amount === null ? "" : entry.amount.toFixed(2)}
+              </td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
