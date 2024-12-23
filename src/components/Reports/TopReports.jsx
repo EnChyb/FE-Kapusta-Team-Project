61269bae1d9f97eb62460a8d1588a9e628078635
KamilAminGoIt <158079851+MKamilAmin@users.noreply.github.com>
2024-12-23 22:44:56 +0100

    🐛 FIX: Correct HTML structure and CSS for TopReports - Changed btns to use svg instead of img - Fixed Balance styles on reports-page - Fixed TopReports styles - Added svgs and removed images

--- src/components/Reports/TopReports.jsx
+++ src/components/Reports/TopReports.jsx
@@ -1,76 +1,99 @@
-import { useState, useEffect } from 'react';
-import './topReport.css';
-import backspaceIcon from '../../assets/images/keyboard_backspace.png';
-import upArrow from '../../assets/images/up-month-arrow.png';
-import backArrow from '../../assets/images/back-month-arrow.png';
-import { Link, useNavigate } from 'react-router-dom';
-import Balance from '../Balance/Balance';
+import { useState, useEffect } from "react";
+import "./topReport.css";
+import { Link, useNavigate } from "react-router-dom";
+import Balance from "../Balance/Balance";
 
 const TopReports = () => {
-  const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(new Date());
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    // Aktualizacja URL z nowym miesiącem
-    // Format YYYY-MM 
-    const formattedMonth = date.toISOString().slice(0, 7); 
-    console.log(formattedMonth)
-    navigate(`/reports/${formattedMonth}`);
-  }, [date, navigate]);
+    useEffect(() => {
+        // Aktualizacja URL z nowym miesiącem
+        // Format YYYY-MM
+        const formattedMonth = date.toISOString().slice(0, 7);
+        console.log(formattedMonth);
+        navigate(`/reports/${formattedMonth}`);
+    }, [date, navigate]);
 
-  const changeMonth = (offset) => {
-    setDate((prevDate) => {
-      const newDate = new Date(prevDate);
-      newDate.setMonth(prevDate.getMonth() + offset);
-      return newDate;
-    });
-  };
+    const changeMonth = (offset) => {
+        setDate((prevDate) => {
+            const newDate = new Date(prevDate);
+            newDate.setMonth(prevDate.getMonth() + offset);
+            return newDate;
+        });
+    };
 
-  const formatDate = () => {
-    const month = date.toLocaleString('en-US', { month: 'long' });
-    const year = date.getFullYear();
-    return `${month} ${year}`;
-  };
+    const formatDate = () => {
+        const month = date.toLocaleString("en-US", { month: "long" });
+        const year = date.getFullYear();
+        return `${month} ${year}`;
+    };
 
-  return (
-    <div className="main-top-report-div">
-      <div className="arrow-div">
-        <Link to="/" className="link-arrow-to-home">
-          <img
-            src={backspaceIcon}
-            alt="Back to Main Page Icon"
-            width="24"
-            height="24"
-          />
-          <p className="main-report-top-txt mobile-disable-txt-arrow">Main page</p>
-        </Link>
-      </div>
-      <div className="balance-period-div">
-      <Balance />
-      </div>
-      <div className="main-current-period-div">
-        <p className="main-report-top-txt">Current period:</p>
-        <div className="show-month-div">
-          <button
-            className="change-month-button"
-            onClick={() => changeMonth(-1)}
-            aria-label="Previous Month"
-          >
-            <img src={backArrow} alt="Previous month" />
-          </button>
-          <p className="second-page-back-txt">{formatDate()}</p>
-          <button
-            className="change-month-button"
-            onClick={() => changeMonth(1)}
-            aria-label="Next Month"
-          >
-            <img src={upArrow} alt="Next month" />
-          </button>
+    // Do przetestowania po naprawieniu styli w ReportsPage.jsx
+    const [isHidden, setIsHidden] = useState(window.innerWidth < 600);
+    useEffect(() => {
+        const handleResize = () => {
+            // console.log(`Window width: ${window.innerWidth}`);
+            setIsHidden(window.innerWidth < 600);
+        };
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    return (
+        <div className="reports-header">
+            <Link to="/" className="reports-header__home-link">
+                <svg width="24" height="24" aria-hidden="true">
+                    <use href="/sprite.svg#back-arrow"></use>
+                </svg>
+                <p
+                    className={
+                        isHidden
+                            ? "reports-header__home-link-text reports-header__home-link-text--visually-hidden"
+                            : "reports-header__home-link-text"
+                    }
+                >
+                    Main page
+                </p>
+            </Link>
+            <div className="reports-header__balance-and-month-selector-container">
+                <Balance />
+
+                <div className="reports-header__month-selector-container">
+                    <p className="reports-header__month-selector-text">
+                        Current period:
+                    </p>
+                    <div className="reports-header__month-selector-nav-wrapper">
+                        <button
+                            className="reports-header__prev-month-btn"
+                            onClick={() => changeMonth(-1)}
+                            aria-label="Previous Month"
+                        >
+                            <svg width="9" height="12" aria-hidden="true">
+                                <use href="/sprite.svg#prev"></use>
+                            </svg>
+                        </button>
+                        <p className="reports-header__current-date">
+                            {formatDate()}
+                        </p>
+                        <button
+                            className="reports-header__next-month-btn"
+                            onClick={() => changeMonth(1)}
+                            aria-label="Next Month"
+                        >
+                            <svg width="9" height="12" aria-hidden="true">
+                                <use href="/sprite.svg#next"></use>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TopReports;
