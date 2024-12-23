4cfe4171d62993a531de3656d88661fd85865184
KamilAminGoIt <158079851+MKamilAmin@users.noreply.github.com>
2024-12-23 01:50:48 +0100

    🐛 FIX: Correct DataHeader styles - Fixed line-height on profile-pic in header - Fixed padding on main html element - Compressed svg for reports link - Added button for mobile in DataHeader.jsx - Fixed DataHeader styling

--- src/components/DataHeader/DataHeader.jsx
+++ src/components/DataHeader/DataHeader.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { Link, useLocation, useSearchParams } from "react-router-dom";
 // import {useState} from "react"
 import Balance from "../Balance/Balance";
@@ -7,41 +7,44 @@ import "./DataHeader.css";
 // import TopReports from '../Reports/TopReports';
 
 const DataHeader = () => {
-	const location = useLocation();
-	const [searchParams, setSearchParams] = useSearchParams();
-	const [date, setDate] = useState(null)
+    const location = useLocation();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [date, setDate] = useState(null);
 
-	const dateParam = searchParams.get('date') ;
+    const dateParam = searchParams.get("date");
 
-	useEffect(() => {
-		if (dateParam) {
-			setDate(new Date(dateParam));
-		} else {
-			setDate(new Date());
-		}
-	}, [dateParam]);
+    useEffect(() => {
+        if (dateParam) {
+            setDate(new Date(dateParam));
+        } else {
+            setDate(new Date());
+        }
+    }, [dateParam]);
 
-	const formattedMonth = date ? date.toISOString().slice(0, 7) : ''; 
+    const formattedMonth = date ? date.toISOString().slice(0, 7) : "";
 
-	useEffect(() => {
-		if (location.pathname.startsWith('/reports') && !dateParam) {
-			setSearchParams({ date: formattedMonth });
-		}
-	}, [dateParam, setSearchParams, formattedMonth, location.pathname]);	
-	console.log(date)
+    useEffect(() => {
+        if (location.pathname.startsWith("/reports") && !dateParam) {
+            setSearchParams({ date: formattedMonth });
+        }
+    }, [dateParam, setSearchParams, formattedMonth, location.pathname]);
+    console.log(date);
 
-	return (
-		<div className="container_dataheader">
-
-					<Balance />
-					<Link className="reportsLinkWrapper" to={`/reports/${formattedMonth}`}>
-					<span className="reports">Reports</span>
-					<svg className="iconReports" width="24" height="24">
-						<use href="/sprite.svg#icon-reports"></use>
-					</svg>
-					</Link>
-		</div>
-	);
+    return (
+        <div className="data-header__container">
+            <button className="data-header__btn">To Transaction</button>
+            <Link
+                className="data-header__link"
+                to={`/reports/${formattedMonth}`}
+            >
+                <span className="data-header__text">Reports</span>
+                <svg className="data-header__icon" width="24" height="24">
+                    <use href="/sprite.svg#reports"></use>
+                </svg>
+            </Link>
+            <Balance />
+        </div>
+    );
 };
 
 export default DataHeader;
