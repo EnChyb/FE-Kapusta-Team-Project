a883c1463244755735bb3b3d6fb3a4319ad65d70
EnChyb <144510772+EnChyb@users.noreply.github.com>
2024-12-28 14:33:41 +0100

    url added in apiConfig

--- api/apiConfig.js
+++ api/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_URL =
   import.meta.env.VITE_API_URL 
-  // || "https://be-kapusta-team-project.onrender.com"
+  || "https://be-kapusta-team-project.onrender.com"
 
 const api = axios.create({
   baseURL: API_URL,
